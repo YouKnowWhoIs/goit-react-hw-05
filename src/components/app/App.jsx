b44da3f3534a6f1f8fae8c617637350f264f86b0
YouKnowWhoIs <leokatmax@gmail.com>
2024-04-11 13:30:23 +0300

    up

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import { Home } from "../pages/home/home.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
